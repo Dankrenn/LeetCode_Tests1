c1b0be9348934f586665d5ffef020d754a3e2925
Danik <Danik@DESKTOP-IPG5FUL>
2023-04-26 18:33:38 +0300

    Добавьте файлы проекта.

---
+++ LeetCode_Tests1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//Дан массив целых чисел nums и целое число target, вернуть индексы двух чисел так, чтобы они в сумме target. 
+public class Program
+{
+    static void Main()
+    {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        int target = int.Parse(Console.ReadLine());
+        int[] ints = (TwoSum(nums, target));
+        Console.WriteLine(ints[0] + " " + ints[1]);
+    }
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> seen = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (seen.ContainsKey(target - nums[i]))
+            {
+                return new int[] { seen[target - nums[i]], i };
+            }
+            if (!seen.ContainsKey(nums[i]))
+            {
+                seen.Add(nums[i], i);
+            }
+
+        }
+        return null;
+    }
+}
+
